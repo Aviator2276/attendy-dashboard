ffeaab33eee3d03e9ca405fddd43b8f415149c6c
Emmett Trippe <ectrippe08@jeffcityschools.org>
2024-10-23 19:00:39 +0000

    created numMembers, getDates, getNumMembersList functions

--- public/js/backend/script.js
+++ public/js/backend/script.js
@@ -3,22 +3,44 @@ import {getData} from "./accessDatabase.js"
 export const main = () => {
     const promise = getData();
     promise.then((data) => {
-        console.log(data)
+        //console.log(data)
 
-        console.log(numMembers('2012-04-23', data));
+        //console.log(numMembers('8/21/2024', data));
+        //console.log(getDates(data))
+        console.log(getNumMembersList(data))
 
     })
 };
 
-export const numMembers = (day, data) => {
-    console.log(data[day])
-}
-export const datesAttended = (user,data) => {
-    for(date in data){
-        for(username in data){
-            if (data[date][username] == user){
-                
-            }
+export const numMembers = (data, date) => {
+    let count = 0
+    for (let i in data) {
+        console.log(date);
+        console.log(data[i][1])
+        if (data[i][1] == date) {
+            count++;
         }
     }
+    return count;
+}
+
+export const getDates = (data) => {
+    const dates = [];
+    
+    for (let i in data) {
+        if (!dates.includes(data[i][1])) {
+            dates.push(data[i][1]);
+        }
+    }
+    return dates
+}
+
+export const getNumMembersList = (data) => {
+    let dates = getDates(data)
+    let numMembersList = {};
+
+    for (let date of dates) {
+        numMembersList[date] = numMembers(data, date);
+    }
+    return numMembersList;
 }
