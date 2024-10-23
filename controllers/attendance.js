e0be5d15a735e1af8785e2fd46695da30b585a06
Aviator 2277 <91645065+Aviator2276@users.noreply.github.com>
2024-10-23 14:25:46 -0400

    Added code to get sheets api

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -1,8 +1,36 @@
 const { StatusCodes } = require('http-status-codes');
+const csv = require('csv-parser');
+const needle = require('needle');
+const urlData =
+  'https://docs.google.com/spreadsheets/d/17bVQrYdBmNAH0in2mje04Gm61WxEfq26JoAiRIB6Orw/gviz/tq?tqx=out:csv&sheet=data';
 const attendanceDB = require('../db/attendance.json');
 
 const getAttendanceList = async (req, res) => {
-  res.status(StatusCodes.OK).json(attendanceDB);
+  const dataRequest = new Promise((resolve, reject) => {
+    const attendanceData = [];
+    needle
+      .get(urlData)
+      .pipe(csv({ headers: false }))
+      .on('data', (data) => {
+        attendanceData.push(data);
+      })
+      .on('done', (err) => {
+        if (err) reject(new Error('Unable to retrieve data.'));
+        else resolve(attendanceData);
+      })
+      .on('end', function () {
+        console.log('Online Database Success.');
+      });
+  });
+
+  try {
+    dataRequest.then((data) => {
+      console.log(data);
+      res.status(StatusCodes.OK).json(data);
+    });
+  } catch (error) {
+    console.error('Unable to retrieve data.');
+  }
 };
 
 module.exports = {
