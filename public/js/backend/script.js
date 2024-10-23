85e27c74156d89e8df391684952416d0c66749c8
Emmett Trippe <ectrippe08@jeffcityschools.org>
2024-10-23 18:15:17 +0000

    fixed accesing database

--- public/js/backend/script.js
+++ public/js/backend/script.js
@@ -2,6 +2,13 @@ import {getData} from "./accessDatabase.js"
 
 
 export const main = () => {
-    const data = getData();
-    console.log(data)
+    const promise = getData();
+    promise.then((data) => {
+        console.log(data)
+
+        for (let i in data) {
+            console.log(data[i])
+        }
+
+    })
 };
