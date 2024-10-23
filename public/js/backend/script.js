a6a893c3c3da01ef36d451e7b5736e7192e15b03
Emmett Trippe <ectrippe08@jeffcityschools.org>
2024-10-23 19:17:34 +0000

    made data a global variable

--- public/js/backend/script.js
+++ public/js/backend/script.js
@@ -1,32 +1,61 @@
-import {getData} from "./accessDatabase.js"
+import { getData } from './accessDatabase.js';
+let data = null;
 
 export const main = () => {
-    const promise = getData();
-    promise.then((data) => {
-        //console.log(data)
+  const promise = getData();
+  promise.then((input_data) => {
+    data = input_data;
+    //console.log(data)
 
-        //console.log(numMembers('8/21/2024', data));
-        //console.log(getDates(data))
-        console.log(getNumMembersList(data))
-
-    })
+    //console.log(numMembers('8/21/2024', data));
+    //console.log(getDates(data))
+    //console.log(getDates())
+    console.log(data)
+    console.log(getNumMembersList());
+    console.log(datesAttended('ahneilson08@jeffcityschools.org'));
+  });
 };
 
-export const numMembers = (day, data) => {
-    console.log(data[day])
-}
+export const numMembers = (date) => {
+  let count = 0;
+  for (let i in data) {
+    if (data[i][1] == date) {
+      count++;
+    }
+  }
+  return count;
+};
+
+export const getDates = () => {
+  const dates = [];
+
+  for (let i in data) {
+    if (!dates.includes(data[i][1])) {
+      dates.push(data[i][1]);
+    }
+  }
+  return dates;
+};
+
+export const getNumMembersList = () => {
+  let dates = getDates();
+  let numMembersList = {};
+
+  for (let date of dates) {
+    numMembersList[date] = numMembers(date);
+  }
+  return numMembersList;
+};
 
 export const datesAttended = (user) => {
   const times = [];
-  const promise = getData();
-  promise.then((data) => {
-    for (let i in data) {
-      let dataObject = data[i];
-      if (dataObject[0] == user) {
-        //0 is the email index
-        times[times.length] = dataObject[1];
-      }
+
+  for (let i in data) {
+    let dataObject = data[i];
+    if (dataObject[0] == user) {
+      times.push(dataObject[1]);
     }
-  });
+  }
+
   return times;
 };
